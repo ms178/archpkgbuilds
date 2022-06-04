d3b3bcc0b4473f5ff0f83a100b735b36fb5d9a33
ms178 <m.seyfarth@gmail.com>
2022-06-04 17:17:04 +0200

    Yet another try
    - with the default LLVM 13.0.1, I now get lld errors late in stage 1, but will post this version anyway as reference

--- toolchain-experimental/llvm-bolt-scripts/build_stage3-bolt.bash
+++ toolchain-experimental/llvm-bolt-scripts/build_stage3-bolt.bash
@@ -16,7 +16,34 @@ echo "== Build with stage2-prof-use-tools -- $CPATH"
 cmake -G Ninja \
     -DLLVM_BINUTILS_INCDIR=/usr/include \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="$(pwd)/install" \
+        -D CMAKE_C_FLAGS=" -O3 -march=native -mllvm -polly -mllvm -polly-position=early -mllvm -polly-parallel=true -fopenmp -fopenmp-version=51 -mllvm -polly-dependences-computeout=5000000 -mllvm -polly-tiling=true -mllvm -polly-prevect-width=256 -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=36 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -g0 -Wp,-D_FORTIFY_SOURCE=0" \
+        -D CMAKE_CXX_FLAGS=" -O3 -march=native -mllvm -polly -mllvm -polly-position=early -mllvm -polly-parallel=true -fopenmp -fopenmp-version=51 -mllvm -polly-dependences-computeout=5000000 -mllvm -polly-tiling=true -mllvm -polly-prevect-width=256 -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=36 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -g0 -Wp,-D_FORTIFY_SOURCE=0" \
+        -D CMAKE_EXE_LINKER_FLAGS="-O3 -march=native -mllvm -polly -mllvm -polly-position=early -mllvm -polly-parallel=true -fopenmp -fopenmp-version=51 -mllvm -polly-dependences-computeout=5000000 -mllvm -polly-tiling=true -mllvm -polly-prevect-width=256 -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=36 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -g0 -Wp,-D_FORTIFY_SOURCE=0 -Wl,-O3,-Bsymbolic-functions,--as-needed" \
+        -D CMAKE_MODULE_LINKER_FLAGS="-O3 -march=native -mllvm -polly -mllvm -polly-position=early -mllvm -polly-parallel=true -fopenmp -fopenmp-version=51 -mllvm -polly-dependences-computeout=5000000 -mllvm -polly-tiling=true -mllvm -polly-prevect-width=256 -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=36 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -g0 -Wp,-D_FORTIFY_SOURCE=0 -Wl,-O3,-Bsymbolic-functions,--as-needed" \
+        -D CMAKE_SHARED_LINKER_FLAGS="-O3 -march=native -mllvm -polly -mllvm -polly-position=early -mllvm -polly-parallel=true -fopenmp -fopenmp-version=51 -mllvm -polly-dependences-computeout=5000000 -mllvm -polly-tiling=true -mllvm -polly-prevect-width=256 -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=36 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -g0 -Wp,-D_FORTIFY_SOURCE=0 -Wl,-O3,-Bsymbolic-functions,--as-needed" \
+        -D CMAKE_INSTALL_PREFIX=/usr \
+        -D LLVM_BINUTILS_INCDIR=/usr/include \
+        -D LLVM_BUILD_LLVM_DYLIB:BOOL=ON \
+        -D LLVM_LINK_LLVM_DYLIB:BOOL=ON \
+        -D CLANG_LINK_CLANG_DYLIB=ON \
+        -D LLVM_BUILD_TOOLS:BOOL=ON \
+        -D LLVM_BUILD_UTILS:BOOL=ON \
+        -D CLANG_ENABLE_ARCMT:BOOL=OFF \
+        -D CLANG_ENABLE_STATIC_ANALYZER:BOOL=OFF \
+        -D COMPILER_RT_BUILD_SANITIZERS:BOOL=OFF \
+        -D COMPILER_RT_BUILD_XRAY:BOOL=OFF \
+        -D LLVM_INCLUDE_BENCHMARKS=OFF \
+        -D LLVM_INCLUDE_GO_TESTS=OFF \
+        -D LLVM_INCLUDE_TESTS=OFF \
+        -D LLVM_INCLUDE_EXAMPLES=OFF \
+        -D LLVM_BUILD_DOCS=OFF \
+        -D LLVM_INCLUDE_DOCS=OFF \
+        -D LLVM_ENABLE_OCAMLDOC=OFF \
+        -D LLVM_ENABLE_SPHINX=OFF \
+        -D LLVM_ENABLE_DOXYGEN=OFF \
+        -D LLVM_ENABLE_BINDINGS=OFF \
+        -D POLLY_ENABLE_GPGPU_CODEGEN=OFF \
+        -D LLVM_ENABLE_Z3_SOLVER=OFF \
     -DCMAKE_AR=${CPATH}/llvm-ar \
     -DCMAKE_C_COMPILER=${CPATH}/clang \
     -DCLANG_TABLEGEN=${CPATH}/clang-tblgen \
@@ -24,17 +51,13 @@ cmake -G Ninja \
     -DLLVM_USE_LINKER=${CPATH}/ld.lld \
     -DLLVM_TABLEGEN=${CPATH}/llvm-tblgen \
     -DCMAKE_RANLIB=${CPATH}/llvm-ranlib \
-    -DLLVM_HOST_TRIPLE=x86_64-unknown-linux \
     -DCMAKE_PREFIX_PATH=${TOPLEV}/stage1/lib/cmake/llvm \
-    -DLLVM_BUILD_RUNTIME=ON \
+    -DLLVM_HOST_TRIPLE=x86_64-unknown-linux \
     -DLLVM_POLLY_LINK_INTO_TOOLS=ON \
     -DLLVM_ENABLE_RUNTIMES="openmp" \
     -DOPENMP_ENABLE_LIBOMPTARGET=OFF \
     -DLLVM_TARGETS_TO_BUILD="AMDGPU;X86" \
     -DLLVM_ENABLE_PROJECTS="clang;lld;polly;compiler-rt;clang-tools-extra" \
-    -D LLVM_BUILD_LLVM_DYLIB=ON \
-    -D LLVM_LINK_LLVM_DYLIB=ON \
-    -D CLANG_LINK_CLANG_DYLIB=ON \
     ../llvm-project/llvm || (echo "Could not configure project!"; exit 1)
 
 echo "== Start Training Build"
