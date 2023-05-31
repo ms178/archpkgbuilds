f3439ea89cd42ad256aecd797d54c732aab12ea7
ms178 <m.seyfarth@gmail.com>
2023-05-31 19:42:32 +0200

    Add files via upload

--- packages/KDE/pkgrel.sh
+++ packages/KDE/pkgrel.sh
@@ -6,12 +6,8 @@ do
   pkgrel=$(awk -F= '/^pkgrel/ {print $2}' "$file" | tr -d ' ')
   # Increment pkgrel
   new_pkgrel=$(echo "$pkgrel + 1" | bc)
-  # Get the current value of pkgver
-  pkgver=$(awk -F= '/^pkgver/ {print $2}' "$file" | tr -d ' ')
   # Build the new version string
   new_version="$new_pkgrel.1"
   # Replace the old pkgrel with the new one
   sed -i "s/^pkgrel=.*/pkgrel=$new_pkgrel/" "$file"
-  # Replace the old pkgver with the new one, including the new version string
-  sed -i "s/^pkgver=.*/pkgver=${pkgver}_${new_version}/" "$file"
 done
