f31c2b5fb0cb24ed866880e5fdcb4c37e936e60f
ms178 <m.seyfarth@gmail.com>
2023-06-09 00:42:37 +0200

    Update pkgrel script
    - to make it more clever

--- packages/KDE/pkgrel.sh
+++ packages/KDE/pkgrel.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-# Find all PKGBUILD files in subdirectories
-find ./ -name PKGBUILD -print0 | while read -d '' -r file;
-do
-  # Get the current value of pkgrel
-  pkgrel=$(awk -F= '/^pkgrel/ {print $2}' "$file" | tr -d ' ')
-  # Increment pkgrel
-  new_pkgrel=$(echo "$pkgrel + 1" | bc)
-  # Build the new version string
-  new_version="$new_pkgrel.1"
-  # Replace the old pkgrel with the new one
-  sed -i "s/^pkgrel=.*/pkgrel=$new_pkgrel/" "$file"
+
+# Iterate over subdirectories
+for dir in */; do
+  cd "$dir" || continue  # Move into the subdirectory
+
+  # Check if PKGBUILD file exists
+  if [[ -f PKGBUILD ]]; then
+    pkgrel=$(grep -oP "(?<=pkgrel=)[^']+" PKGBUILD)  # Extract pkgrel value
+
+    # Check if pkgrel value has dot and 1
+    if [[ $pkgrel =~ \.1$ ]]; then
+      # Split the pkgrel value into two parts
+      IFS='.' read -r -a parts <<< "$pkgrel"
+
+      # Increment the first part
+      new_pkgrel=$((parts[0] + 1))
+
+      # Create the new_pkgrel value with dot and 1
+      new_pkgrel="${new_pkgrel}.1"
+
+      # Replace pkgrel value in PKGBUILD
+      sed -i "s/pkgrel=$pkgrel/pkgrel=$new_pkgrel/" PKGBUILD
+    else
+      # Increment pkgrel value by 1
+      new_pkgrel=$((pkgrel + 1))
+
+      # Replace pkgrel value in PKGBUILD
+      sed -i "s/pkgrel=$pkgrel/pkgrel=$new_pkgrel/" PKGBUILD
+    fi
+
+    echo "Updated PKGBUILD in $dir"
+  fi
+
+  cd ..  # Move back to the parent directory
 done
