a3fdffab5f36ae93fc9ad3115707cedd5eb850aa
ms178 <m.seyfarth@gmail.com>
2023-09-29 13:51:21 +0200

    Also BOLT lld from now on
    - thnks to ChatGPT for the help
    - LLD stats:
    
            148482592609 : executed forward branches (-5.5%)
             11569235205 : taken forward branches (-69.5%)
             45459705845 : executed backward branches (+32.5%)
             21556571340 : taken backward branches (+1.7%)
              6157453439 : executed unconditional branches (-52.3%)
             14146589919 : all function calls (-19.5%)
              3149880130 : indirect calls (-45.7%)
                       0 : PLT calls (-100.0%)
           1260965019395 : executed instructions (-0.7%)
            285881044687 : executed load instructions (-0.1%)
            153290236340 : executed store instructions (=)
              1615900708 : taken jump table branches (=)
                       0 : taken unknown indirect branches (=)
            200099751893 : total branches (-2.1%)
             39283259984 : taken branches (-45.5%)
            160816491909 : non-taken conditional branches (+21.6%)
             33125806545 : taken conditional branches (-44.0%)
            193942298454 : all conditional branches (+1.3%)

--- toolchain-experimental/llvm-bolt-scripts-master/build_stage3-bolt-without-sampling.bash
+++ toolchain-experimental/llvm-bolt-scripts-master/build_stage3-bolt-without-sampling.bash
@@ -3,28 +3,39 @@
 TOPLEV=~/toolchain/llvm
 cd ${TOPLEV}
 
-mkdir -p ${TOPLEV}/stage3-without-sampling/intrumentdata || (echo "Could not create stage3-bolt directory"; exit 1)
+mkdir -p ${TOPLEV}/stage3-without-sampling/instrumentdata || (echo "Could not create stage3-bolt directory"; exit 1)
 cd ${TOPLEV}/stage3-without-sampling
 CPATH=${TOPLEV}/stage2-prof-use-lto/install/bin
 BOLTPATH=${TOPLEV}/llvm-bolt/bin
 
-
-echo "Instrument clang with llvm-bolt"
-
+echo "Instrument Clang with llvm-bolt"
 ${BOLTPATH}/llvm-bolt \
     --lite=false \
     --instrument \
     --instrumentation-file-append-pid \
-    --instrumentation-file=${TOPLEV}/stage3-without-sampling/intrumentdata/clang-18.fdata \
+    --instrumentation-file=${TOPLEV}/stage3-without-sampling/instrumentdata/clang-18.fdata \
     ${CPATH}/clang-18 \
     -o ${CPATH}/clang-18.inst
 
-echo "mooving instrumented binary"
+echo "Moving instrumented Clang binary"
 mv ${CPATH}/clang-18 ${CPATH}/clang-18.org
 mv ${CPATH}/clang-18.inst ${CPATH}/clang-18
 
+echo "Instrument LLD with llvm-bolt"
+${BOLTPATH}/llvm-bolt \
+    --lite=false \
+    --instrument \
+    --instrumentation-file-append-pid \
+    --instrumentation-file=${TOPLEV}/stage3-without-sampling/instrumentdata/lld-18.fdata \
+    ${CPATH}/lld \
+    -o ${CPATH}/lld.inst
+
+echo "Moving instrumented LLD binary"
+mv ${CPATH}/lld ${CPATH}/lld.org
+mv ${CPATH}/lld.inst ${CPATH}/lld
+
 echo "== Configure Build"
-echo "== Build with stage2-prof-use-lto instrumented clang -- $CPATH"
+echo "== Build with stage2-prof-use-lto instrumented Clang and LLD -- $CPATH"
 
 cmake -G Ninja ../llvm-project/llvm \
     -DCMAKE_BUILD_TYPE=Release \
@@ -32,7 +43,7 @@ cmake -G Ninja ../llvm-project/llvm \
     -DLLVM_TARGETS_TO_BUILD="X86" \
     -DLLVM_ENABLE_PROJECTS="polly;lld;clang;compiler-rt" \
     -D CMAKE_C_FLAGS="-O3 -march=native -mtune=native -maes -mbmi2 -mpclmul -mllvm -inline-threshold=500 -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-enable-delicm=true -mllvm -extra-vectorizer-passes -mllvm -enable-interleaved-mem-accesses -mllvm -enable-masked-interleaved-mem-accesses -mllvm -enable-cond-stores-vec -mllvm -slp-vectorize-hor-store -mllvm -enable-loopinterchange -mllvm -enable-loop-distribute -mllvm -enable-unroll-and-jam -mllvm -enable-loop-flatten -mllvm -interleave-small-loop-scalar-reduction -mllvm -unroll-runtime-multi-exit -mllvm -aggressive-ext-opt -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fomit-frame-pointer -mllvm -vp-counters-per-site=6 -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables -freroll-loops -mllvm -adce-remove-loops -mllvm -enable-ext-tsp-block-placement=1 -mllvm -enable-gvn-hoist -mllvm -enable-dfa-jump-thread" \
-    -D CMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -maes -mbmi2 -mpclmul -mllvm -inline-threshold=500 -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-enable-delicm=true -mllvm -extra-vectorizer-passes -mllvm -enable-interleaved-mem-accesses -mllvm -enable-masked-interleaved-mem-accesses -mllvm -enable-cond-stores-vec -mllvm -slp-vectorize-hor-store -mllvm -enable-loopinterchange -mllvm -enable-loop-distribute -mllvm -enable-unroll-and-jam -mllvm -enable-loop-flatten -mllvm -interleave-small-loop-scalar-reduction -mllvm -unroll-runtime-multi-exit -mllvm -aggressive-ext-opt -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fomit-frame-pointer -mllvm -vp-counters-per-site=6 -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables -freroll-loops -mllvm -adce-remove-loops -mllvm -enable-ext-tsp-block-placement=1 -mllvm -enable-gvn-hoist -mllvm -enable-dfa-jump-thread" \
+    -D CMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -maes -mbmi2 -mpclmul -mllvm -inline-threshold=500 -mllvm -polly-invariant-load-hoisting -mllvm -polly-loopfusion-greedy -mllvm -polly-run-inliner -mllvm -polly-run-dce -mllvm -polly-enable-delicm=true -mllvm -extra-vectorizer-passes -mllvm -enable-interleaved-mem-accesses -mllvm -enable-masked-interleaved-mem-accesses -mllvm -enable-cond-stores-vec -mllvm -slp-vectorize-hor-store -mllvm -enable-loopinterchange -mllvm -enable-loop-distribute -mllvm -enable-unroll-and-jam -mllvm -enable-loop-flatten -mllvm -interleave-small-loop-scalar-reduction -mllvm -unroll-runtime-multi-exit -mllvm -aggressive-ext-opt -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -fomit-frame-pointer -mllvm -vp-counters-per-site=6 -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables" \
     -D CMAKE_EXE_LINKER_FLAGS="-Wl,--lto-CGO3 -Wl,--gc-sections -Wl,--icf=all -Wl,--lto-O3,-O3,-Bsymbolic-functions,--as-needed -march=native -mtune=native -maes -mbmi2 -mpclmul -fuse-ld=lld -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables" \
     -D CMAKE_MODULE_LINKER_FLAGS="-Wl,--lto-CGO3 -Wl,--gc-sections -Wl,--icf=all -Wl,--lto-O3,-O3,-Bsymbolic-functions,--as-needed -march=native -mtune=native -maes -mbmi2 -mpclmul -fuse-ld=lld -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables" \
     -D CMAKE_SHARED_LINKER_FLAGS="-Wl,--lto-CGO3 -Wl,--gc-sections -Wl,--icf=all -Wl,--lto-O3,-O3,-Bsymbolic-functions,--as-needed -march=native -mtune=native -maes -mbmi2 -mpclmul -fuse-ld=lld -fcf-protection=none -mharden-sls=none -flto=thin -fwhole-program-vtables" \
@@ -47,9 +58,9 @@ echo "== Start Training Build"
 ninja & read -t 360 || kill $!
 
 echo "Merging generated profiles"
-cd ${TOPLEV}/stage3-without-sampling/intrumentdata
+cd ${TOPLEV}/stage3-without-sampling/instrumentdata
 LD_PRELOAD=/usr/lib/libjemalloc.so ${BOLTPATH}/merge-fdata *.fdata > combined.fdata
-echo "Optimizing Clang with the generated profile"
+echo "Optimizing Clang and LLD with the generated profile"
 
 LD_PRELOAD=/usr/lib/libjemalloc.so ${BOLTPATH}/llvm-bolt ${CPATH}/clang-18.org \
     --data combined.fdata \
@@ -67,6 +78,24 @@ LD_PRELOAD=/usr/lib/libjemalloc.so ${BOLTPATH}/llvm-bolt ${CPATH}/clang-18.org \
     -group-stubs -align-blocks -sctc-mode=heuristic -jump-tables=aggressive -simplify-conditional-tail-calls -simplify-rodata-loads \
     -align-macro-fusion=all -eliminate-unreachable -tail-duplication=cache -indirect-call-promotion=all -icp-eliminate-loads \
     -hot-data -x86-strip-redundant-address-size -lite=false -reorder-data-algo=funcs -inline-memcpy -cg-from-perf-data \
-    -plt=hot|| (echo "Could not optimize binary for clang"; exit 1)
+    -plt=hot || (echo "Could not optimize Clang binary"; exit 1)
 
-echo "You can now use the compiler with export PATH=${CPATH}:${PATH}"
+LD_PRELOAD=/usr/lib/libjemalloc.so ${BOLTPATH}/llvm-bolt ${CPATH}/lld.org \
+    --data combined.fdata \
+    -o ${CPATH}/lld \
+    -reorder-blocks=ext-tsp \
+    -reorder-functions=cds \
+    -split-functions \
+    -split-all-cold \
+    -split-eh \
+    -dyno-stats \
+    -icf=1 \
+    -use-gnu-stack \
+    -peepholes=all \
+    -elim-link-veneers \
+    -group-stubs -align-blocks -sctc-mode=heuristic -jump-tables=aggressive -simplify-conditional-tail-calls -simplify-rodata-loads \
+    -align-macro-fusion=all -eliminate-unreachable -tail-duplication=cache -indirect-call-promotion=all -icp-eliminate-loads \
+    -hot-data -x86-strip-redundant-address-size -lite=false -reorder-data-algo=funcs -inline-memcpy -cg-from-perf-data \
+    -plt=hot || (echo "Could not optimize LLD binary"; exit 1)
+
+echo "You can now use the optimized Clang and LLD with export PATH=${CPATH}:${PATH}"
