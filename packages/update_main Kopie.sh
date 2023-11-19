b5f7c42799e060f8f75c043f9e5e7a7a7e9cdd76
ms178 <m.seyfarth@gmail.com>
2023-11-19 11:58:36 +0100

    Add appstream
    ... needed by frameworkintegration and plasma-workspace.

--- packages/update_main Kopie.sh
+++ packages/update_main Kopie.sh
@@ -3,7 +3,6 @@
 # You need to adjust the path in the first line to the folder where you placed all of these packages and make this script executable with "chmod +x". I haven't found a way to say "Yes" automatically to pacman, hence you need to install all of the following packages manually in the correct order when installing them first, but you can use this script for updates. Execute it with "./update.sh". Beware of the dangers, the build order is important and this is now tested to be the correct order. As I personally use Kwinft-main and compile that seperatly, you need that package and its dependancies (wrapland-main, disman-main, kdisplay-main) in addition to this list to be installed. You can integrate these packages above plasma-workspace-main in this script if you like.
 cd /home/marcus/Downloads/KDE
 for pkg in extra-cmake-modules-main \
-oxygen-sounds-main \
 kdnssd-main \
 ki18n-main \
 solid-main \
@@ -56,7 +55,6 @@ frameworkintegration-main \
 kdeclarative-main \
 kcmutils-main \
 breeze-main \
-breeze-gtk-main \
 kactivities-main \
 kactivities-stats-main \
 kactivitymanagerd-main \
@@ -91,8 +89,8 @@ networkmanager-qt-main \
 phonon-main \
 plasma-integration-main \
 kpipewire-main \
-kdisplay-kwinft \
-kwinft \
+kscreen-main \
+kwin-main \
 plasma-workspace-main \
 kde-cli-tools-main \
 systemsettings-main \
@@ -126,7 +124,7 @@ kaccounts-integration-main \
 kio-extras-main \
 kwalletmanager-main \
 signon-kwallet-extension-main \
-spectacle-main \
+spectacle-main
 ; do
   cd $pkg
   rm -f *.zst
