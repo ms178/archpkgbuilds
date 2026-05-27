386585a3239eb1079fe4c143aa6e9802b4919296
ms178 <m.seyfarth@gmail.com>
2026-05-27 11:14:47 +0200

    Add files via upload

--- packages/mpv-git/04_SAT1.sh
+++ packages/mpv-git/04_SAT1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-mpv "rtsp://192.168.178.1:554/?avm=1&freq=466&bw=8&msys=dvbc&mtype=256qam&sr=6900&specinv=1&pids=0,16,17,18,20,7620,7622,7623,7624,7628,7629" --rtsp-transport=udp --deinterlace
+# SAT.1 via FritzBox 6660 SAT>IP (DVB-C)
+# Freq 410 MHz (from FritzBox M3U)
+# PIDs: 7140=PMT, 7142=Video, 7143/7144=Audio, 7148/7149=Subs/Teletext
+
+mpv --profile=dvbc \
+  --deinterlace \
+  "rtsp://192.168.178.1:554/?avm=1&freq=410&bw=8&msys=dvbc&mtype=256qam&sr=6900&specinv=1&pids=0,16,17,18,20,7140,7142,7143,7144,7148,7149"
